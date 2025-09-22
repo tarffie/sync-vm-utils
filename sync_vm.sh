3665704a2d85bac7e622558de4284b0c44506dfa
Emilia <emiliaalves.tarffie@gmail.com>
2025-09-22 11:03:10 -0300

    fix: made a mistake adding --help and --version to sync_vm, will update it later

--- sync_vm.sh
+++ sync_vm.sh
@@ -1,34 +1,9 @@
 #!/bin/sh
 
-show_help() {
-  echo "Usage: sync_vm <target_directory> <ip_address>"
-  echo "Options:"
-  echo "  --help  Display this message"
-  echo "  --version Show the script version"
-}
-
 if [ "$#" -ne 2 ]; then
-  show_help() exit 1
+  echo "Usage: sync_vm <target_directory> <ip_address>"
+  exit 1 
 fi
-
-while [[ "$1" != "" ]]; do
-  case $1 in
-  --help)
-    show_help
-    exit
-    ;;
-  --version)
-    echo "sync_vm Version 1.0"
-    exit
-    ;;
-  *)
-    echo "Invalid option: $1"
-    show_help
-    exit 1
-    ;;
-  esac
-  shift
-done
 
 TARGET_DIR="$1"
 IP_ADDR="$2"
